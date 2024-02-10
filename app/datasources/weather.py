2a90e0b2706c8b8bbb9fef7c41e1f498cbcbf66e
fablukm <fablukm@gmail.com>
2024-02-10 22:58:19 +0100

    api call works

--- app/datasources/weather.py
+++ app/datasources/weather.py
@@ -2,31 +2,42 @@ from data import ProcessedData, DataSource
 from typing import List, Dict
 import json
 from pathlib import Path
+from dataclasses import dataclass
 import requests
+
+@dataclass
+class Location:
+    name: str
+    timezone: str
+    lat: float
+    lon: float
 
 class WeatherDataSource(DataSource):
     def read_location_config(self) -> List[Dict]: 
         """Read locations from the config file"""
-        config_path = Path(__file__).parent.parent / 'configs/locations.json'
+        config_path = Path(__file__).parent.parent.parent / 'configs/locations.json'
         with config_path.open('r') as file:
             locations = json.load(file)
         return locations
     
     def read_api_key(self) -> Dict:
-        config_path = Path(__file__).parent.parent / 'configs/api_keys.json'
+        config_path = Path(__file__).parent.parent.parent / 'configs/api_keys.json'
         with config_path.open('r') as file:
             api_keys = json.load(file)
         osm_api_key = [value for value in api_keys if value["service"]=="OpenWeatherMaps"][0]
-        return osm_api_key
+        return osm_api_key["key"]
     
-    def fetch_weather_for_location(self, name, timezone):
-        api_key = "YOUR_API_KEY"
+    def fetch_weather_for_location(self, location):
+        api_key = self.read_api_key()
         base_url = "http://api.openweathermap.org/data/2.5/weather"
         params = {
-            "q": name,
+            "lat": location["lat"],
+            "lon": location["lon"],
             "appid": api_key,
-            "units": "metric"
+            "units": "metric",
+            "exclude": "daily,minutely,alerts",
         }
+
         response = requests.get(base_url, params=params)
         if response.status_code == 200:
             return response.json()
@@ -35,6 +46,17 @@ class WeatherDataSource(DataSource):
 
     def fetch_data(self) -> List[Dict]:
         """Fetch the raw data from an external source."""
+        locations = self.read_location_config()
+
+        weather_results = []
+        for loc in locations:
+            response = self.fetch_weather_for_location(loc)
+            result = {
+                "name": loc["name"],
+                "raw_info": loc,
+                "weather": response
+                }
+            
         pass
 
     def process_data(self, raw_data: List[Dict]) -> List[Dict]:
@@ -49,4 +71,4 @@ class WeatherDataSource(DataSource):
     
 if __name__=="__main__":
     wds = WeatherDataSource()
-    print(wds.read_api_key())
+    print(wds.fetch_data())
